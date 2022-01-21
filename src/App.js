f902164f477ed4b99beed5a16b28428119756256
Borjaarmet <borjaarmet@gmail.com>
2022-01-21 09:43:22 +0100

    setup structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs : </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button className="btn">Generate</button>
+      </form>
+      <article>
+        <p>
+          Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+          Carrot cake caramels pie sweet apple pie tiramisu carrot cake.
+          Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon
+          drops bear claw gummies.{" "}
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
